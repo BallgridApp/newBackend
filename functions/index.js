52ddc4be82fc7a6b40f4a7be0ec6e5fdd0c61544
Rahul Ravi <ravirahul906@gmail.com>
2022-07-24 16:04:17 -0400

    file addition

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,10 @@
 const functions = require("firebase-functions");
 const yo = require('./yo')
-
+const match = require('./MatchEndpoints');
 
 
 exports.yo = yo.app;
-
+exports.match = match.match
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
